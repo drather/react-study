2d6a38b0c98d89b82789ec37c9dd1544099fb7e6
kks <drather@naver.com>
2024-03-11 00:05:54 +0900

    fix: not a function 에러 수정

--- src/component/list/CommentList.jsx
+++ src/component/list/CommentList.jsx
@@ -16,9 +16,11 @@ const Wrapper = styled.div`
 `;
 
 function CommentList(props) {
+    const {comments, onClickItem} = props;
+    
     return (
         <Wrapper>
-            {CommentList.map((comment, index) => {
+            {comments.map((comment, index) => {
                 return <CommentListItem key={comment.id} comment={comment} />;
             })}
         </Wrapper>
